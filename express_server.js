1c1ebf814ced000eeaddd362bfd9106d944e674e
Lucas Tan <sheng.x.tan@gmail.com>
2022-03-22 01:14:29 +0000

    created a url_show/ejs and added a new route to express_server.js

--- express_server.js
+++ express_server.js
@@ -31,3 +31,10 @@ app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
+
+app.get("/urls/:shortURL", (req, res) => {
+  console.log(req.params);
+  console.log(req.params.shortURL);
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
+  res.render("urls_show", templateVars);
+});
